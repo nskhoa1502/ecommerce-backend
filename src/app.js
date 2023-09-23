1bf785710442e6dda632874f0fb5dd1589e228e4
haha <nskhoa1502@gmail.com>
2023-09-23 16:20:17 +0700

    install-basic-package

--- src/app.js
+++ src/app.js
@@ -1,12 +1,27 @@
+const compression = require("compression");
 const express = require("express");
+const { default: helmet } = require("helmet");
+const morgan = require("morgan");
 
 const app = express();
 
 // Init middlewares
 
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+
 // init database
 
 // init router
+app.get("/", (req, res, next) => {
+  //   const strCompress = "Hello World";
+
+  return res.status(200).json({
+    // message: "strCompress",
+    // metadata: strCompress.repeat(10000),
+  });
+});
 
 // handling errors
 
